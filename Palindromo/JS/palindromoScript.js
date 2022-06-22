674fc1fbf5819987dae0fb45d881a967e4c33ff6
Vegadrone <spenaantonino@gmail.com>
2022-06-22 14:47:16 +0200

    Update: add alert ok and comments

--- Palindromo/JS/palindromoScript.js
+++ Palindromo/JS/palindromoScript.js
@@ -1,1 +1,13 @@
-alert ('ok')
+//!################### CONSEGNA ###################
+
+// * Palidroma
+// * Chiedere all’utente di inserire una parola.
+// * Creare una funzione per capire se la parola inserita è palindroma.
+
+// TODO Consigli del giorno
+// TODO Scriviamo sempre in italiano i passaggi che vogliamo fare
+// TODO Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+
+//?################### ESERCIZIO ###################
+
+alert('ok')
---
+++ Pari e Dispari/JS/pariDispari.js
@@ -0,0 +1,15 @@
+//!################### CONSEGNA ###################
+
+// * L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// * Generiamo un numero random(sempre da 1 a 5) per il computer(usando una funzione).
+// * Sommiamo i due numeri
+// * Stabiliamo se la somma dei due numeri è pari o dispari(usando una funzione)
+// * Dichiariamo chi ha vinto.
+
+// TODO Consigli del giorno
+// TODO Scriviamo sempre in italiano i passaggi che vogliamo fare
+// TODO Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+
+//?################### ESERCIZIO ###################
+
+alert('ok')
